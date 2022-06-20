fce9e98db99e5269b71a667942fbf53dc5c71e8a
NickZemlin <agnight010998@gmail.com>
2022-06-21 02:11:29 +0300

    export panel polishing

--- src/components/ui/exportPanel.vue
+++ src/components/ui/exportPanel.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 import { useAppStore } from "../../store/AppStore";
 
 const appStore = useAppStore();
@@ -10,15 +11,31 @@ const expandText = computed(() => {
 });
 
 const settingsPresenter = computed(() => {
-  const result = [];
-  if (appStore.settings) {
-    let settings = appStore.settings;
-    for (let item = 0; item < Object.keys(settings).length; item++) {
-      let field = Object.keys(settings)[item] as keyof typeof settings;
-      result.push({ header: field, setting: settings[field] });
-    }
+  return appStore.getFormatedSettings();
+});
+
+const textToCopy = computed(() => {
+  let map = settingsPresenter.value.map(
+    (line) => `${line.header} ${line.setting} \n`
+  );
+  map.unshift((animationName.value as string) + "\n");
+  return map.reduce((acc, line) => (acc += line));
+});
+
+const copyAnimation = ref(false);
+const copySettings = () => {
+  if (!copyAnimation.value) {
+    copyAnimation.value = true;
+    window.setTimeout(() => {
+      copyAnimation.value = false;
+    }, 700);
   }
-  return result;
+  navigator.clipboard.writeText(textToCopy.value);
+};
+
+const route = useRoute();
+const animationName = computed(() => {
+  return route.name;
 });
 </script>
 
@@ -29,7 +46,7 @@ const settingsPresenter = computed(() => {
     </div>
     <transition name="export">
       <div class="export-settings-wrap" v-if="expand">
-        <h3>Settings:</h3>
+        <h3 class="export-settings-name">{{ animationName }}</h3>
         <div class="export-settings-content">
           <div
             class="export-settings-setting"
@@ -37,6 +54,24 @@ const settingsPresenter = computed(() => {
           >
             <h4 style="user-select: text">{{ settings.header }}</h4>
             <p style="user-select: text">{{ settings.setting }}</p>
+          </div>
+          <div class="export-settings-copy" @click="copySettings">
+            <transition-group name="text">
+              <p
+                class="export-settings-copy-text"
+                key="copy"
+                v-if="!copyAnimation"
+              >
+                copy
+              </p>
+              <p
+                class="export-settings-copy-text"
+                key="copied"
+                v-if="copyAnimation"
+              >
+                copied
+              </p>
+            </transition-group>
           </div>
         </div>
       </div>
@@ -71,14 +106,34 @@ const settingsPresenter = computed(() => {
   &-settings {
     &-wrap {
       box-sizing: border-box;
-      width: 150px;
+      width: 300px;
       padding: 10px 0px;
       padding-left: 10px;
       background-color: #f2f3f5;
       border-radius: 0px 0px 0px 10px;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+    &-name {
+      text-align: center;
+      margin-bottom: 20px;
     }
     &-setting {
       margin-bottom: 5px;
+    }
+    &-copy {
+      height: 30px;
+      margin-top: 10px;
+      margin-left: 25%;
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #333;
+      cursor: pointer;
+      &-text {
+        color: white;
+      }
     }
   }
 }
@@ -88,14 +143,14 @@ const settingsPresenter = computed(() => {
   width: 0;
 }
 .export-enter-to {
-  width: 150px;
+  width: 300px;
 }
 .export-enter-active {
   transition: all 0.6s cubic-bezier(0.87, 0, 0.13, 1);
 }
 
 .export-leave-from {
-  width: 150px;
+  width: 300px;
 }
 .export-leave-to {
   padding: 0;
@@ -103,5 +158,29 @@ const settingsPresenter = computed(() => {
 }
 .export-leave-active {
   transition: all 0.6s cubic-bezier(0.87, 0, 0.13, 1);
+}
+
+.text-enter-from {
+  transform: translateY(15px);
+  opacity: 0;
+}
+
+.text-enter-active {
+  transition: all 0.3s ease-out 0.3s;
+  position: absolute;
+}
+.text-leave-to {
+  transform: translateY(-15px);
+  opacity: 0;
+}
+
+.text-leave-active {
+  transition: all 0.3s ease-in;
+  position: absolute;
+}
+
+.setting-enter-from {
+  transform: translateY(15px);
+  opacity: 0;
 }
 </style>
--- src/store/AppStore.ts
+++ src/store/AppStore.ts
@@ -1,5 +1,21 @@
 import { defineStore } from "pinia";
 
+const settingsType = {
+  duration: "time",
+  enterDuration: "time",
+  enterDelay: "time",
+  moveDuration: "time",
+  moveDelay: "time",
+  delay: "time",
+  exitDuration: "time",
+  opacityClosed: "value",
+  opacityOpened: "value",
+  xAxis: "valuePx",
+  yAxis: "valuePx",
+  easingEnter: "timingFunction",
+  easingLeave: "timingFunction",
+};
+
 export interface Settings {
   duration?: number;
   enterDuration?: number;
@@ -30,6 +46,34 @@ export const useAppStore = defineStore("main", {
   actions: {
     updateSettings(settings: Settings) {
       this.settings = settings;
+    },
+    getFormatedSettings() {
+      let result: Array<{ header: string; setting: string }> = [];
+      let settings = this.settings;
+      if (settings !== null) {
+        for (let item = 0; item < Object.keys(settings).length; item++) {
+          let valueType = Object.keys(settings)[item];
+          let value =
+            settings[Object.keys(settings)[item] as keyof typeof settings];
+          let type =
+            settingsType[
+              Object.keys(settings)[item] as keyof typeof settingsType
+            ];
+          switch (type) {
+            case "time":
+              value = Number(value) / 100 + "s";
+              break;
+            case "valuePx":
+              value = Number(value) + "px";
+              break;
+          }
+          result.push({
+            header: Object.keys(settings)[item] + ":",
+            setting: value!.toString(),
+          });
+        }
+      }
+      return result;
     },
   },
 });
