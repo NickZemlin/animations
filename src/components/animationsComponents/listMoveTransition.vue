173cda75781faa3cc616b22c6bffb64035362d80
NickZemlin <agnight010998@gmail.com>
2022-06-20 21:26:02 +0300

    clean up

--- src/components/animationsComponents/listMoveTransition.vue
+++ src/components/animationsComponents/listMoveTransition.vue
@@ -9,94 +9,86 @@ import { useRoute } from "vue-router";
 import CustomSlider from "../ui/CustomSlider.vue";
 import { timingFunctions } from "../../animationsList/animationsList";
 
-
-
-
-
-
 const description = computed(() => {
   return route.meta.description as string;
 });
 
 const route = useRoute();
 
-const show = ref(true);
-const toggleAnimation = () => {
-  show.value = !show.value;
-};
-
 const shuffleArray = (input: exampleItem[]) => {
-  let currentIndex = input.length,  randomIndex;
+  let currentIndex = input.length,
+    randomIndex;
   while (currentIndex != 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
     [input[currentIndex], input[randomIndex]] = [
-      input[randomIndex], input[currentIndex]];
+      input[randomIndex],
+      input[currentIndex],
+    ];
   }
+};
 
-}
-
-const sortDescending = (input: exampleItem[]) =>{
-    // input.sort((a , b)=>a.displayNum - b.displayNum)
-    return (input.sort((a , b)=>a.displayNum - b.displayNum))
-}
+const sortDescending = (input: exampleItem[]) => {
+  return input.sort((a, b) => a.displayNum - b.displayNum);
+};
 
 class exampleArray extends Array {
-    public array!: exampleItem[]
-    public customSort(){
-        return sortDescending(this.array)
+  public array!: exampleItem[];
+  public customSort() {
+    return sortDescending(this.array);
+  }
+  public shuffle() {
+    shuffleArray(this.array);
+  }
+  public addNew() {
+    if (this.array.length) {
+      const temp = [...this.array].sort((a, b) => a.displayNum - b.displayNum);
+      this.array.push(new exampleItem(temp[temp.length - 1].displayNum + 1));
+    } else {
+      this.array.push(new exampleItem(1));
     }
-    public shuffle(){
-        shuffleArray(this.array)
-    }
-    public addNew(){
-        if (this.array.length){
-            const temp = [...this.array]
-        this.array.push(new exampleItem(temp[temp.length - 1].displayNum +1))
-
-        }else {
-            this.array.push(new exampleItem(1))
-        }
-    }
-    public removeLast(){
-        this.array.pop()
-    }
-    constructor(amount: number){
-        super()
-        this.array = generateExample(amount)
-    }
+  }
+  public removeFirst() {
+    this.array.shift();
+  }
+  public removeLast() {
+    this.array.pop();
+  }
+  constructor(amount: number) {
+    super();
+    this.array = generateExample(amount);
+  }
 }
 
 class exampleItem {
-    public displayNum!: number
-    public key!: number
+  public displayNum!: number;
+  public key!: number;
 
-    constructor(display: number){
-        this.displayNum = display
-        this.key = display
-    }
+  constructor(display: number) {
+    this.displayNum = display;
+    this.key = display;
+  }
 }
 
-const generateExample = (amount: number) =>{
-    const output = []
-    for (let i = 1; i < amount + 1; i++){
-        output.push(new exampleItem(i))
-    }
-    return output
-}
+const generateExample = (amount: number) => {
+  const output = [];
+  for (let i = 1; i < amount + 1; i++) {
+    output.push(new exampleItem(i));
+  }
+  return output;
+};
 
-const renderArray = ref(new exampleArray(10))
-
+const renderArray = ref(new exampleArray(10));
 
 interface Settings {
   enterDuration: number;
   enterDelay: number;
   exitDuration: number;
-  moveDuration: number
+  moveDuration: number;
   moveDelay: number;
   easingEnter: string;
   easingLeave: string;
-  easingMove: string
+  easingMove: string;
 }
 
 const settings = ref<Settings>({
@@ -106,8 +98,8 @@ const settings = ref<Settings>({
   moveDuration: 75,
   moveDelay: 0,
   easingEnter: "ease",
-  easingLeave: "ease", 
-  easingMove: "ease"
+  easingLeave: "ease",
+  easingMove: "ease",
 });
 
 const updateTimingFunction = (value: string, intent: "in" | "out" | "move") => {
@@ -119,36 +111,40 @@ const updateTimingFunction = (value: string, intent: "in" | "out" | "move") => {
       settings.value.easingLeave = value;
       break;
     case "move":
-        settings.value.easingMove = value
+      settings.value.easingMove = value;
   }
 };
 
-const enterTiming = computed(()=>{
-    return `all ${settings.value.enterDuration / 100}s ${settings.value.easingEnter} ${settings.value.enterDelay/100}s`
-})
+const enterTiming = computed(() => {
+  return `all ${settings.value.enterDuration / 100}s ${
+    settings.value.easingEnter
+  } ${settings.value.enterDelay / 100}s`;
+});
 
-const exitTiming = computed(()=>{
-    return `all ${settings.value.exitDuration / 100}s ${settings.value.easingLeave}`
-})
+const exitTiming = computed(() => {
+  return `all ${settings.value.exitDuration / 100}s ${
+    settings.value.easingLeave
+  }`;
+});
 
-const moveTiming = computed(()=>{
-    return `all ${settings.value.moveDuration/100}s ${settings.value.easingMove} ${settings.value.moveDelay/100}s`
-})
-
-
+const moveTiming = computed(() => {
+  return `all ${settings.value.moveDuration / 100}s ${
+    settings.value.easingMove
+  } ${settings.value.moveDelay / 100}s`;
+});
 </script>
 
 <template>
-<ControlPanel>
-    <CustomButton @click="toggleAnimation">play</CustomButton>
+  <ControlPanel>
     <CustomButton @click="renderArray.shuffle()">shuffle</CustomButton>
     <CustomButton @click="renderArray.customSort()">sort</CustomButton>
     <CustomButton @click="renderArray.addNew()">add new</CustomButton>
+    <CustomButton @click="renderArray.removeFirst()">remove first</CustomButton>
     <CustomButton @click="renderArray.removeLast()">remove last</CustomButton>
   </ControlPanel>
-   <teleport to="#Settings">
+  <teleport to="#Settings">
     <CustomDescription :text="description" />
-        <CustomSlider
+    <CustomSlider
       v-model="settings.enterDuration"
       :value="settings.enterDuration"
       :max="100"
@@ -180,7 +176,7 @@ const moveTiming = computed(()=>{
       :default="settings.moveDuration"
       :min="0"
     />
-        <CustomSlider
+    <CustomSlider
       v-model="settings.moveDelay"
       :value="settings.moveDelay"
       :max="100"
@@ -198,61 +194,58 @@ const moveTiming = computed(()=>{
       header="Timing Function out"
       @update="updateTimingFunction($event, 'out')"
     />
-        <CustomRadio
+    <CustomRadio
       :selection="timingFunctions"
       header="Timing Function move"
       @update="updateTimingFunction($event, 'move')"
     />
-    </teleport>
-        <transition-group name="list" tag="div" class="example-wrap">
-        <div class="example" v-for="li in renderArray.array" :key="li.key" >
-        {{li.displayNum}}
-        </div>
-        </transition-group>
+  </teleport>
+  <transition-group name="list" tag="div" class="example-wrap">
+    <div class="example" v-for="li in renderArray.array" :key="li.key">
+      {{ li.displayNum }}
+    </div>
+  </transition-group>
 </template>
 
-
 <style scoped lang="scss">
-
-.example{
-    &-wrap{
-        display: flex;
-        flex-direction: row;
-        align-content: flex-start;
-    }
-    width: 50px;
-    height: 50px;
-    margin-right: 5px;
-    margin-top: 5px;
-    background-color: #333;
+.example {
+  &-wrap {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    will-change: transition;
+    flex-direction: row;
+    align-content: flex-start;
+  }
+  width: 50px;
+  height: 50px;
+  margin-right: 5px;
+  margin-top: 5px;
+  background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  will-change: transition;
 }
 
-.list-enter-from{
-    opacity: 0;
+.list-enter-from {
+  opacity: 0;
 }
-.list-enter-to{
-    opacity: 1;
+.list-enter-to {
+  opacity: 1;
 }
-.list-enter-active{
-    transition: v-bind(enterTiming);
-}
-
-.list-leave-to{
-    opacity: 0;
-    transform: translateX(-50px);
-}
-.list-leave-active{
-    transition: v-bind(exitTiming);
-    position: absolute;
+.list-enter-active {
+  transition: v-bind(enterTiming);
 }
 
-.list-move{
-   transition: v-bind(moveTiming);
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(-50px);
+}
+.list-leave-active {
+  transition: v-bind(exitTiming);
+  position: absolute;
 }
 
+.list-move {
+  transition: v-bind(moveTiming);
+}
 </style>
